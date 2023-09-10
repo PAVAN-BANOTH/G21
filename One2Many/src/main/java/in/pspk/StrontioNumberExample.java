fc603104198023f9bac4f9907a628a2e1fa36118
PAVAN-BANOTH <banothpavan901@gmail.com>
2023-09-10 22:37:06 +0530

    SecCommit

--- One2Many/src/main/java/in/pspk/StrontioNumberExample.java
+++ One2Many/src/main/java/in/pspk/StrontioNumberExample.java
@@ -1,5 +1,21 @@
 package in.pspk;
 
-public class StrontioNumberExample {
+import java.util.Scanner;
 
+public class StrontioNumberExample {//10th100th digits same
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int num = scanner.nextInt();
+        scanner.close();
+
+        int n = num;
+        int firstDigit = (num * 2) % 1000 / 10;
+
+        if (firstDigit % 10 == firstDigit / 10) {
+            System.out.println(n + " is a Strontio number.");
+        } else {
+            System.out.println(n + " is not a Strontio number.");
+        }
+    }
 }
